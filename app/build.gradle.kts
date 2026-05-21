53cbacef0a7b558bff431360fe4794485546333b
Jay Mee <jamied_uk@users.noreply.github.com>
2026-05-21 09:34:30 +0100

    Bump version to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jnetai.worldtimes"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "v1.0.0"
+        versionCode = 2
+        versionName = "v1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
